5ebc685e96713b7638a3fb20650e3f31b3038066
AnikHaque <ekramul15-811@diu.edu.bd>
2023-09-12 16:43:42 +0600

    updated home screen title

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Home'),
     );
   }
 }
@@ -47,11 +47,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
+        title: Text(
+          widget.title,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true, // Center the title horizontally.
       ),
+
       body: Center(
 
         child: Column(
@@ -59,12 +65,63 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'This is mod 5 Assignment',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'My ',
+                    style: TextStyle(
+                      color: Colors.pink, // Color for "My"
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'phone ',
+                    style: TextStyle(
+                      color: Colors.blue, // Color for "phone"
+                      fontWeight: FontWeight.bold,
+                        fontSize: 13
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'name ',
+                    style: TextStyle(
+                      color: Colors.pink, // Color for "My"
+                      fontWeight: FontWeight.bold,
+                        fontSize: 13
+                    ),
+
+                  ),
+                  TextSpan(
+                    text: 'Your ',
+                    style: TextStyle(
+                      color: Colors.orange, // Color for "phone"
+                     fontSize: 16
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'phone ',
+                    style: TextStyle(
+                      color: Colors.orange,
+                        fontSize: 16
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'name',
+                    style: TextStyle(
+                      color: Colors.orange,
+                        fontSize: 16
+                    ),
+                  ),
+                ],
+              ),
+            )
+
           ],
         ),
       ),
